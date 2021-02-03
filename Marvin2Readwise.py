9c2f286952ae7da915e63e08897cd316ce3c0d53
thecatalogueback <78452690+thecatalogueback@users.noreply.github.com>
2021-02-03 11:18:28 +1000

    Update Marvin2Readwise.py

--- Marvin2Readwise.py
+++ Marvin2Readwise.py
@@ -1,5 +1,5 @@
 # Python3
-## version 03-02-2021
+## version 03-02-2021a
 # Marvin Export to Readwise
 
 import csv, sys, json
@@ -9,25 +9,27 @@ def ReadwiseExporter(MarvinCsv, ExportCsv):
   with open(MarvinCsv) as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-      rows.append(row)
+      if row.get('HighlightText') or row.get('EntryText'):
+        rows.append(row)
 
-  #print(json.dumps(rows)) # DEBUG
+  print(json.dumps(rows)) # DEBUG
 
   with open(ExportCsv, 'w', newline='') as csvfile:
     writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
     writer.writerow(['Highlight', 'Title', 'Author', 'URL', 'Note', 'Location'])
     for row in rows:
-      if not row.get('HighlightText'):
-        row['HighlightText'] = 'VOCAB: ' + row.get('EntryText') # ASSUMES is Highlighted Vocab
-        row['EntryText'] = '' # Clear Entry Text as not to get duplicate
-      writer.writerow([
-        row['HighlightText'].replace('\n', ''),
-        row['Title'].replace('\n', ''),
-        row['Author'].replace('\n', ''),
-        '',
-        row['EntryText'].replace('\n', ''),
-        ''
-      ])
+      if row.get('HighlightText') or row.get('EntryText'): # Valid Entry
+        if not row.get('HighlightText') and row.get('EntryText'):
+          row['HighlightText'] = 'VOCAB: ' + row.get('EntryText') # ASSUMES is Highlighted Vocab
+          row['EntryText'] = '' # Clear Entry Text as not to get duplicate
+        writer.writerow([
+          row['HighlightText'].replace('\n', '    '),
+          row['Title'].replace('\n', '    '),
+          row['Author'].replace('\n', '    '),
+          '',
+          row['EntryText'].replace('\n', '    '),
+          ''
+        ])
 
   print('...Done')
 
